0744889e0532bf8ae9fb2ac1e3fdfee7251ceae6
takasu-atsuki <jill.perry0208@ilcoud.com>
2023-04-02 13:27:19 +0900

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from '@mui/system';
-import { Typography, Link } from '@mui/material';
+import { Typography, Link, Paper, Box } from '@mui/material';
 
 function Copyright(props) {
   return (
@@ -19,17 +19,43 @@ function Copyright(props) {
 
 const Footer = () => {
   return (
-    <Container
-      maxWidth="md"
-      component="footer"
+    // <Container
+    //   maxWidth="md"
+    //   component="footer"
+    //   sx={{
+    //     borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+    //     mt: 8,
+    //     py: [3, 6],
+    //   }}
+    // >
+    //   <Copyright sx={{ mt: 5 }} />
+    // </Container>
+    <Paper
       sx={{
-        borderTop: (theme) => `1px solid ${theme.palette.divider}`,
-        mt: 8,
-        py: [3, 6],
+        marginTop: 'calc(10% + 60px)',
+        width: '100%',
+        position: 'fixed',
+        bottom: 0,
       }}
+      component="footer"
+      square
+      variant="outlined"
     >
-      <Copyright sx={{ mt: 5 }} />
-    </Container>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: 'center',
+            display: 'flex',
+            mb: 2,
+          }}
+        >
+          <Typography variant="caption" color="initial">
+            Copyright ©2023.
+          </Typography>
+        </Box>
+      </Container>
+    </Paper>
   );
 };
 export default Footer;
